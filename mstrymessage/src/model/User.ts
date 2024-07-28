da532dcaa89da2ea184adab21b7b5d0cd3d52115
kunalarya873 <kunalarya873@gmail.com>
2024-07-28 21:23:03 +0530

    Stoping project for sometime

---
+++ mstrymessage/src/model/User.ts
@@ -0,0 +1,10 @@
+import mongoose, {Schema, Document} from "mongoose";
+
+export interface Message extends Document {
+    content: string;
+    createdAt : Date;
+}
+
+const MessageSchema: Schema<Message> = new Schema({
+    content: String,
+})
