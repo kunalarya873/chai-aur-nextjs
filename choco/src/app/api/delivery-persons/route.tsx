9d9551d2d022c6d7ddfb5f76f66dfdd4239c58be
kunalarya873 <kunalarya873@gmail.com>
2024-11-05 13:46:42 +0530

    Get all delievery persons

--- choco/src/app/api/delivery-persons/route.tsx
+++ choco/src/app/api/delivery-persons/route.tsx
@@ -1,6 +1,7 @@
 import { db } from "@/lib/db/db";
-import { deliveryPersons } from "@/lib/db/schemas";
+import { deliveryPersons, warehouses } from "@/lib/db/schemas";
 import { deliveryPersonSchema } from "@/lib/validators/deliveryPersonSchema";
+import { desc, eq } from "drizzle-orm";
 
 export async function POST(request: Request) {
     const requestData = await request.json();
@@ -18,5 +19,24 @@ export async function POST(request: Request) {
         return new Response(JSON.stringify({message: 'Delivery person stored successfully'}), {status: 201});
     } catch (error) {
         return new Response(JSON.stringify({message: `Failed to store delivery person: ${error}`}), {status: 500});
+    }
+}
+
+export async function GET() {
+    try {
+        const alldeliveryPersons = await db.select({
+            id: deliveryPersons.id,
+            name: deliveryPersons.name,
+            phone: deliveryPersons.phone,
+            warehouse: warehouses.name
+        })
+        .from(deliveryPersons)
+        .leftJoin(warehouses, eq(deliveryPersons.warehouseId, warehouses.id))
+        .orderBy(desc(deliveryPersons.id))
+        .execute();
+        return new Response(JSON.stringify(alldeliveryPersons), {status: 200});
+    }
+    catch (error) {
+        return new Response(JSON.stringify({message: `Failed to fetch delivery persons: ${error}`}), {status: 500});
     }
 }
