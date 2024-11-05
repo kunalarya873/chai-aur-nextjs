f0bc70fe1c6304d6bbb89a5b029c3a7b27736f5f
kunalarya873 <kunalarya873@gmail.com>
2024-11-05 19:19:37 +0530

    get all inventory feature added

--- choco/src/app/api/inventories/route.tsx
+++ choco/src/app/api/inventories/route.tsx
@@ -1,6 +1,8 @@
 import { inventorySchema } from "@/lib/validators/inventorySchema";
 import { db } from "@/lib/db/db";
-import { inventories } from "@/lib/db/schemas";
+import { inventories, products, warehouses } from "@/lib/db/schemas";
+import { desc, eq } from "drizzle-orm";
+
 export async function POST(request: Request) {
     const requestData = await request.json();
     
@@ -17,5 +19,24 @@ export async function POST(request: Request) {
     } catch (error) {
         return new Response(JSON.stringify({message: "Failed to store inventory " + error}), {status: 500});
         
+    }
+}
+
+export async function GET() {
+    try {
+        const allinventories = await db.select({
+            id: inventories.id,
+            sku: inventories.sku,
+            warehouse: warehouses.name,
+            product: products.name
+        })
+        .from(inventories)
+        .leftJoin(warehouses, eq(inventories.warehouseId, warehouses.id))
+        .leftJoin(products, eq(inventories.productId, products.id))
+        .orderBy(desc(inventories.id))
+        .execute();
+        return new Response(JSON.stringify(allinventories), {status: 200});
+    } catch (error) {
+        return new Response(JSON.stringify({message: `Failed to fetch all inventories: ${error}`}), {status: 500});
     }
 }
